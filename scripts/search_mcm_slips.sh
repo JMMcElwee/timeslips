310a07cacf82cb797901f68574d699dcac45d0a2
JMcElwee <jmmcelwee1@sheffield.ac.uk>
2020-04-14 16:53:21 +0100

    Updating environment variables

--- scripts/search_mcm_slips.sh
+++ scripts/search_mcm_slips.sh
@@ -1,12 +1,13 @@
+VALID=""
 for arg in "$@"
 do 
     if [[ $arg == "-v" ]] 
     then 
-	VALIDMCM="-validate"
+	VALID="-validate"
 	echo -e "\e[34;1m[INFO]\e[0m Validating slips."
     else
 	echo -e "\e[33;1m[ERROR]\e[0m Unknown argument... just going to ignore it."
     fi
 done
 
-python TotalAnalysis.py -slip MCMtoCTM  -root smoothed_plots.root $VALIDMCM
+python TotalAnalysis.py -slip MCMtoCTM -root smoothed_plots.root $VALID
