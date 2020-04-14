51a28ecb5fa860be571e22478ccebce9e61c48ae
JMcElwee <jmmcelwee1@sheffield.ac.uk>
2020-04-14 14:40:30 +0100

    Update analysis scripts

--- scripts/gen_upload_script.sh
+++ scripts/gen_upload_script.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd tables/
+cd tables
+
+if [[ ! -z $ND280SYS ]]
+then
+    echo -e "\e[34;1m[INFO]\e[0m \$ND280SYS set to" $ND280SYS
+    # Find the upload script. Saves on hardcoding the position
+    UPLOADER=$(find $ND280SYS/oaOfflineDatabase/*/Linux* -name "database_updater.py")
+else 
+    echo -e "\e[31;1m[ERROR]\e[0m Please set \$ND280SYS to point at your ND280 build."
+    return;
+fi
 
 cat > UploadFiles.sh <<EOF
 #!/bin/bash
@@ -9,7 +19,7 @@ EOF
 
 for f in ./*.dat; do
 cat >> UploadFiles.sh <<EOF
-python $CMTPATH/oaOfflineDatabase/v*r*/Linux-x86_64/database_updater.py --convert_unsigned apply_local_update $f
+python $UPLOADER --convert_unsigned apply_local_update $f
 EOF
 done
 
